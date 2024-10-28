edcedbea7460815b9647965049d5d065d3abda34
Jesus Angel <jesus.angel.9703@gmail.com>
2024-10-28 10:43:39 -0500

    Limit tip to 20

--- projects/tip-calculator/index.js
+++ projects/tip-calculator/index.js
@@ -6,8 +6,16 @@ const totalSpan = document.getElementById("total");
 function calculateTotal() {
   const billValue = billInput.value;
   const tipValue = tipInput.value;
-  const totalValue = billValue * (1 + tipValue / 100);
-  totalSpan.innerText = totalValue.toFixed(2);
+  if(tipValue<20){
+    const totalValue = billValue * (1 + tipValue / 100);
+    totalSpan.innerText = totalValue.toFixed(2);
+  }
+  else{
+    totalValue = String("The tip is abusive");
+    totalSpan.innerText= totalValue;
+  }
+    
+  
 }
 
 btnEl.addEventListener("click", calculateTotal);
