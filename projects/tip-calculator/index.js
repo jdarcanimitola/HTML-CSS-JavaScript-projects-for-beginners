5d698b5345a1e947fe4f60cfba8c8a32552e8d9a
Jesus Angel <jesus.angel.9703@gmail.com>
2024-10-28 13:12:18 -0500

    Add clean button and split the total calculation

--- projects/tip-calculator/index.js
+++ projects/tip-calculator/index.js
@@ -1,21 +1,39 @@
-const btnEl = document.getElementById("calculate");
+const btnCalculate = document.getElementById("calculate");
+const btnClean = document.getElementById("clean");
 const billInput = document.getElementById("bill");
 const tipInput = document.getElementById("tip");
+const subtotalSpan = document.getElementById("subtotal");
+const finalTipSpan = document.getElementById("final-tip");
 const totalSpan = document.getElementById("total");
 
 function calculateTotal() {
   const billValue = billInput.value;
   const tipValue = tipInput.value;
   if(tipValue<20){
+    const finalTipValue = billValue * tipValue / 100;
     const totalValue = billValue * (1 + tipValue / 100);
+    subtotalSpan.innerText = billValue;
+    finalTipSpan.innerText = finalTipValue.toFixed(2);
     totalSpan.innerText = totalValue.toFixed(2);
   }
   else{
+    subtotalSpan.innerText = billValue;
+    finalTipValue = "The tip is abusive";
+    finalTipSpan.innerText = finalTipValue;
     totalValue = "The tip is abusive";
-    totalSpan.innerText= totalValue;
+    totalSpan.innerText = totalValue;
   }
     
   
 }
 
-btnEl.addEventListener("click", calculateTotal);
+function cleanTotal() {
+billInput.value=null;
+tipInput.value=null;
+subtotalSpan.innerText = "";
+finalTipSpan.innerText = "";
+totalSpan.innerText = "";
+}
+
+btnCalculate.addEventListener("click", calculateTotal);
+btnClean.addEventListener("click", cleanTotal);
