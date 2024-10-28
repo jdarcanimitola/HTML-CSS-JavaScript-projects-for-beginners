351c268666017a6ec01af7c521f2b0542f5082a4
Jesus Angel <jesus.angel.9703@gmail.com>
2024-10-28 14:45:47 -0500

    Change bill amount for product/price table

--- projects/tip-calculator/index.js
+++ projects/tip-calculator/index.js
@@ -1,34 +1,37 @@
 const btnCalculate = document.getElementById("calculate");
 const btnClean = document.getElementById("clean");
-const billInput = document.getElementById("bill");
+const productOneInput = document.getElementById("product-one");
+const productTwoInput = document.getElementById("product-two");
+const priceOneInput = document.getElementById("price-one");
+const priceTwoInput = document.getElementById("price-two");
 const tipInput = document.getElementById("tip");
 const subtotalSpan = document.getElementById("subtotal");
 const finalTipSpan = document.getElementById("final-tip");
 const totalSpan = document.getElementById("total");
 
 function calculateTotal() {
-  const billValue = billInput.value;
+  const priceOneValue = 0 + priceOneInput.value;
+  const priceTwoValue = 0 + priceTwoInput.value;
+  const billValue = parseFloat(priceOneValue) + parseFloat(priceTwoValue);
   const tipValue = tipInput.value;
+  subtotalSpan.innerText = billValue;
   if(tipValue<20){
     const finalTipValue = billValue * tipValue / 100;
     const totalValue = billValue * (1 + tipValue / 100);
-    subtotalSpan.innerText = billValue;
     finalTipSpan.innerText = finalTipValue.toFixed(2);
     totalSpan.innerText = totalValue.toFixed(2);
   }
   else{
-    subtotalSpan.innerText = billValue;
-    finalTipValue = "The tip is abusive";
-    finalTipSpan.innerText = finalTipValue;
-    totalValue = "The tip is abusive";
-    totalSpan.innerText = totalValue;
+    finalTipSpan.innerText = "The tip is abusive";
+    totalSpan.innerText = "The tip is abusive";
   }
-    
-  
 }
 
 function cleanTotal() {
-billInput.value=null;
+productOneInput.value=null;
+productTwoInput.value=null;
+priceOneInput.value=null;
+priceTwoInput.value=null;
 tipInput.value=null;
 subtotalSpan.innerText = "";
 finalTipSpan.innerText = "";
