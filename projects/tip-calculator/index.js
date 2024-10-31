bacf083504ceca3534aa439403602d6f7a4fe1f8
Jesus Angel <jesus.angel.9703@gmail.com>
2024-10-31 10:19:31 -0500

    Split subtotal behavior from button

--- projects/tip-calculator/index.js
+++ projects/tip-calculator/index.js
@@ -1,57 +1,73 @@
+const productOneInput = document.getElementById("product-one");
+const priceOneInput = document.getElementById("price-one");
+const unitOneInput = document.getElementById("unit-one");
+const moneyOneSpan = document.getElementById("money-one");
+
+const productTwoInput = document.getElementById("product-two");
+const priceTwoInput = document.getElementById("price-two");
+const unitTwoInput = document.getElementById("unit-two");
+const moneyTwoSpan = document.getElementById("money-two");
+
+const tipInput = document.getElementById("tip");
+
 const btnCalculate = document.getElementById("calculate");
 const btnClean = document.getElementById("clean");
-const productOneInput = document.getElementById("product-one");
-const productTwoInput = document.getElementById("product-two");
-const priceOneInput = document.getElementById("price-one");
-const priceTwoInput = document.getElementById("price-two");
-const unitOneInput = document.getElementById("unit-one");
-const unitTwoInput = document.getElementById("unit-two");
-const tipInput = document.getElementById("tip");
-const moneyOneSpan = document.getElementById("money-one");
-const moneyTwoSpan = document.getElementById("money-two");
+
 const subtotalSpan = document.getElementById("subtotal");
 const finalTipSpan = document.getElementById("final-tip");
 const totalSpan = document.getElementById("total");
 
+function calculateFirst() {
+  const priceOneValue = priceOneInput.value;
+  const unitOneValue = unitOneInput.value;
+  const priceOneTotalValue = priceOneValue * unitOneValue;
+  moneyOneSpan.innerText = priceOneTotalValue;
+}
+
 function calculateTotal() {
   const priceOneValue = priceOneInput.value;
-  const priceTwoValue = priceTwoInput.value;
   const unitOneValue = unitOneInput.value;
+  const priceOneTotalValue = priceOneValue * unitOneValue;
+
+  const priceTwoValue = priceTwoInput.value;
   const unitTwoValue = unitTwoInput.value;
-  const priceOneTotalValue = priceOneValue*unitOneValue;
-  const priceTwoTotalValue = priceTwoValue*unitTwoValue;
-  moneyOneSpan.innerText = priceOneTotalValue;
-  moneyTwoSpan.innerText = priceTwoTotalValue;
-  
+  const priceTwoTotalValue = priceTwoValue * unitTwoValue;
+
   const billValue = priceOneTotalValue + priceTwoTotalValue;
-  const tipValue = tipInput.value;
   subtotalSpan.innerText = billValue;
-  if(tipValue<20){
-    const finalTipValue = billValue * tipValue / 100;
+
+  const tipValue = tipInput.value;
+  232;
+  if (tipValue < 20) {
+    const finalTipValue = (billValue * tipValue) / 100;
     const totalValue = billValue * (1 + tipValue / 100);
     finalTipSpan.innerText = finalTipValue.toFixed(2);
     totalSpan.innerText = totalValue.toFixed(2);
-  }
-  else{
+  } else {
     finalTipSpan.innerText = "The tip is abusive";
     totalSpan.innerText = "The tip is abusive";
   }
 }
 
 function cleanTotal() {
-productOneInput.value=null;
-productTwoInput.value=null;
-priceOneInput.value=null;
-priceTwoInput.value=null;
-unitOneInput.value=null;
-unitTwoInput.value=null;
-moneyOneSpan.innerText="";
-moneyTwoSpan.innerText="";
-tipInput.value=null;
-subtotalSpan.innerText = "";
-finalTipSpan.innerText = "";
-totalSpan.innerText = "";
+  productOneInput.value = null;
+  priceOneInput.value = null;
+  unitOneInput.value = null;
+  moneyOneSpan.innerText = "";
+
+  productTwoInput.value = null;
+  priceTwoInput.value = null;
+  unitTwoInput.value = null;
+  moneyTwoSpan.innerText = "";
+
+  tipInput.value = null;
+  subtotalSpan.innerText = "";
+  finalTipSpan.innerText = "";
+  totalSpan.innerText = "";
 }
+
+priceOneInput.addEventListener("focusout", calculateFirst);
+unitOneInput.addEventListener("focusout", calculateFirst);
 
 btnCalculate.addEventListener("click", calculateTotal);
 btnClean.addEventListener("click", cleanTotal);
